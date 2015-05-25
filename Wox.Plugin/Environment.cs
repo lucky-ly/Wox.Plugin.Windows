7ef92b7934e06790f2c7045313c33d562ee6baf1
Дмитрий Свечников <d.svechnikov@gmail.com>
2015-05-25 22:22:24 +0500

    Init

---
+++ Wox.Plugin/Environment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wox.Plugin.Environment
+{
+	public class Environment : IPlugin
+	{
+		public List<Result> Query(Query query)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Init(PluginInitContext context)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
